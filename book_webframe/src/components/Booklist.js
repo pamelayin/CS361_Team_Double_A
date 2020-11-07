f169d840b18ba0e6577ff5433c3d3bd12b457eb9
Pamela Yin <44883345+pamelayin@users.noreply.github.com>
2020-11-06 22:59:37 -0800

    update pakcage.json, readme, booklist.js

--- book_webframe/src/components/Booklist.js
+++ book_webframe/src/components/Booklist.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
-
+import { Button } from 'reactstrap';
+import ReactFlexyTable from 'react-flexy-table';
+import 'react-flexy-table/dist/index.css';
+// import '../App.css';
 export class Booklist extends Component
 {
     render(){
         return(
-            <div>
-                <h1>Book List</h1>
-				<p>These are the books available for request.</p>
-				<table>Books Available</table>
+            <div class="jumbotron jumbotron-fluid booklist">
+                <div class="container">
+                    <h1 class="display-4"> Book List</h1>
+				    <p class="lead">These are the books available on our platform available for request.</p>
+                </div>
             </div>
+        
         )
     }
 }
