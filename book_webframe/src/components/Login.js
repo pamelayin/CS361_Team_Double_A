ce17d155c29bc899e1deb1ee1020e14e68e9d1da
jaywangjhw <55812968+jaywangjhw@users.noreply.github.com>
2020-11-08 12:34:19 -0800

    Updated login and signin

--- book_webframe/src/components/Login.js
+++ book_webframe/src/components/Login.js
@@ -12,11 +12,13 @@ export class Login extends Component
                     <div class="col-md-6">
                         <div class="card">
                             <form class="box">
-                                <h1>Login</h1>
+                                <h1>Sign In</h1>
                                 <p class="text-muted"> Please enter your login and password!</p> 
-                                <input type="text" name="" placeholder="Username"></input>
+                                <input type="text" name="" placeholder="Username/Email"></input>
                                 <input type="password" name="" placeholder="Password"></input> 
-                                <a class="forgot text-muted" href="#">Forgot password?</a> <input type="submit" name="" value="Login" href="#"></input>
+                                <a class="forgot text-muted" href="#">Forgot password?</a> 
+                                <input type="submit" name="" value="Login" href="#"></input>
+                                <input type="submit" name="" value="Create New Account" href="#"></input>
                                 <div class="col-md-12">
                                     <ul class="social-network social-circle">
                                         <li><a href="#" class="icoFacebook" title="Facebook"><i class="fab fa-facebook-f"></i></a></li>
