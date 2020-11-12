b36c7a6559d4c01939b978a497480ae18921f767
Pamela Yin <44883345+pamelayin@users.noreply.github.com>
2020-11-12 00:27:28 -0800

    databse setup, schema setup

---
+++ book_webframe/backend/routes/books.js
@@ -0,0 +1,53 @@
+const router = require("express").Router();
+let Book = require("../models/book.model");
+
+router.route("/").get((req, res) => {
+	Book.find()
+		.then((books) => res.json(books))
+		.catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/add").post((req, res) => {
+	const isbn = req.body.isbn;
+	const title = req.body.title;
+	const author = req.body.author;
+	const publishing_date = Date(req.body.publishing_date);
+	const posting_user = req.body.posting_user;
+	const condition = req.body.condition;
+	const book_points = Number(req.body.book_points);
+	const available = Boolean(req.body.available);
+	const image = req.body.image;
+	const requested = Boolean(req.body.swap.requested);
+	const accepted = Boolean(req.body.swap.accepted);
+	const shipped = Boolean(req.body.swap.shipped);
+	const received = Boolean(req.body.swap.received);
+	const request_date = Date(req.body.swap.request_date);
+	const requesting_user = req.body.swap.requesting_user;
+
+	const newBook = new Book({
+		isbn,
+		title,
+		author,
+		publishing_date,
+		posting_user,
+		condition,
+		book_points,
+		available,
+		image,
+		swap: {
+			requested,
+			accepted,
+			shipped,
+			received,
+			request_date,
+			requesting_user,
+		},
+	});
+
+	newBook
+		.save()
+		.then(() => res.json("Book added!"))
+		.catch((err) => res.status(400).json("Error: " + err));
+});
+
+module.exports = router;
