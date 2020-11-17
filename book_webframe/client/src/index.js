5fe10e8adda968de899f4f15d8066c5b65d93d49
jaywangjhw <55812968+jaywangjhw@users.noreply.github.com>
2020-11-16 22:36:24 -0800

    Login/Logout/Create Account

--- book_webframe/client/src/index.js
+++ book_webframe/client/src/index.js
@@ -5,9 +5,17 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.css'
 //import MainPage2 from './components/MainPage'
 
+import { Auth0Provider } from "@auth0/auth0-react";
+
 ReactDOM.render(
-  <App/>,
-  document.getElementById('root')
+  <Auth0Provider
+    domain="dev-cd29zu3y.us.auth0.com"
+    clientId="n28Biun64j5BsM2EDTAUB2ZaJvh96M1k"
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
