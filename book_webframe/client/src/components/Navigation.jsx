9122bda2eccf51ce9eba035688982a6e07487bbc
jaywangjhw <55812968+jaywangjhw@users.noreply.github.com>
2020-11-21 18:27:06 -0800

    Fixed signup error

--- book_webframe/client/src/components/Navigation.jsx
+++ book_webframe/client/src/components/Navigation.jsx
@@ -71,7 +71,7 @@ const NavBar = () => {
                     exact
                     activeClassName="router-link-exact-active"
                   >
-                    External API
+                     
                   </NavLink>
                 </NavItem>
               )}
